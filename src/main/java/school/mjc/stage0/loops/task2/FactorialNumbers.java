341b199d46c979d90fe52614847ba37b7d123f00
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-27 10:54:52 +0500

    z

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -7,8 +7,13 @@ public class FactorialNumbers {
             System.out.println(1);
         }else {
         int a =1;
+        int t=0;
         int k=1;
         while (a<=printToInclusive){
+            if (t==0){
+                System.out.println(1);
+                t++;
+            }
             k=k*a;
             a++;
             System.out.println(k);
